06819dcbde57ce2b2bb8fc8d9db7d4aa757362f2
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-05-19 15:05:35 +0600

    Handle payment processing error and success

--- src/Pages/Dashboard/CheckOutForm.js
+++ src/Pages/Dashboard/CheckOutForm.js
@@ -1,44 +1,62 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React,{useState} from 'react';
 
 const CheckOutForm = () => {
     const stripe = useStripe();
     const elements = useElements()
-    const handleSubmit = async (event) =>{
+    const [cardError,setCardError] = useState('')
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         if (!stripe || !elements) {
             // Stripe.js has not loaded yet. Make sure to disable
             // form submission until Stripe.js has loaded.
             return;
-          }
-          const card = elements.getElement(CardElement);
+        }
+        const card = elements.getElement(CardElement);
 
-          if (card == null) {
+        if (card == null) {
             return;
-          }
+        }
+
+        // Use your card Element with other Stripe.js APIs
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+        if(error){
+            setCardError(error?.message || '')
+        }
+
     }
     return (
-        <form onSubmit={handleSubmit}>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: '16px',
-                color: '#424770',
-                '::placeholder': {
-                  color: '#aab7c4',
-                },
-              },
-              invalid: {
-                color: '#9e2146',
-              },
-            },
-          }}
-        />
-        <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
-          Pay
-        </button>
-      </form>
+        <>
+                    <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+                Pay
+            </button>
+        </form>
+        {
+            cardError && <p className='text-red-500'></p>
+        }
+        </>
     );
 };
 
---
+++ src/Pages/Dashboard/_payments_step.js
@@ -0,0 +1,10 @@
+/**
+ * 1. install react-stripe-js
+ * 2. open stripe account on stripe website
+ * 3. get publihsable key pk_
+ * 4. create elements wrapper using publishable key
+ * 5. create checkout form using card element, useStripe, useElements
+ * 6. get card elements info(credit card info)
+ * ...............
+ * 
+ */
